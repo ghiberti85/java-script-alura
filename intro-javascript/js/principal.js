26c3d7c1b405053cb8eb9f658e14a1cb93bcf5b8
Fernando Ghiberti <ghiberti@Fernandos-MacBook-Pro.local>
2020-02-21 23:27:05 -0300

    Adicionando Pasta intro-javascript

---
+++ intro-javascript/js/principal.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++) {
+
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoValido = true;
+  var alturaValida = true;
+
+  if(peso <= 0 || peso >= 500) {
+    pesoValido = false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(altura <= 0 || altura >= 3.00) {
+    alturaValida = false;
+    tdImc.textContent = "Altura Inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (alturaValida && pesoValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+
+}
