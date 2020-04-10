6238214397642bd0fe198cf4042b40bde8ec8155
Fernando Ghiberti <ghiberti@Fernandos-MacBook-Pro.local>
2020-04-10 19:47:33 -0300

    Adicionando Plugins Jquery

--- jQuery-typer/public/js/placar.js
+++ jQuery-typer/public/js/placar.js
@@ -1,6 +1,8 @@
+$("#botao-sync").click(sincronizaPlacar);
+
 function inserePlacar() {
   var corpoTabela = $(".placar").find("tbody");
-  var usuario = "Fernando";
+  var usuario = $("#usuarios").val();
   var numPalavras = $("#contador-palavras").text();
 
   var linha = novaLinha(usuario, numPalavras);
@@ -59,4 +61,52 @@ function scrollPLacar() {
     {
       scrollTop: posicaoPlcar + "px"
     }, 1000);
+}
+
+function sincronizaPlacar(){
+
+  var placar = [];
+  var linhas = $("tbody>tr");
+
+  //novo
+  linhas.each(function(){
+      var usuario = $(this).find("td:nth-child(1)").text();
+      var palavras = $(this).find("td:nth-child(2)").text();
+
+      var score = {
+          usuario: usuario,
+          pontos: palavras            
+      };
+
+      placar.push(score); //guardando o score no array
+
+  });
+
+      var dados = {
+        placar: placar
+    };
+
+    $.post("http://localhost:3000/placar", dados , function() {
+      console.log("Placar sincronizado com sucesso");
+        $(".tooltip").tooltipster("open"); 
+      }).fail(function(){
+          $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
+        }).always(function(){ 
+            setTimeout(function() {
+            $(".tooltip").tooltipster("close"); 
+            },1200);
+          });
+
+}
+
+function atualizaPlacar(){
+  $.get("http://localhost:3000/placar",function(data){
+      $(data).each(function(){
+          var linha = novaLinha(this.usuario, this.pontos);
+
+          linha.find(".botao-remover").click(removeLinha);
+
+          $("tbody").append(linha);
+      });
+  });
 }
